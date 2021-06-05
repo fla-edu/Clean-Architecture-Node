44487512e28db600eeab4ee288fc7d255077c73d
Flávio Eduardo Oliveira E Silva <flavio.eduardo@b2mlportal.com.br>
2021-06-05 19:31:30 -0300

    test: ensure SignUpController returns 400 if no passwordConfirmation is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -44,17 +44,17 @@ describe('SignUp Controller', () => {
     expect(httpReponse.body).toEqual(new MissingParamError('password'))
   })
 
-  // test('Should return 400 if no name is provided', () => {
-  //   const sut = new SignUpController()
-  //   const httpRequest = {
-  //     body: {
-  //       name: 'any_name',
-  //       password: 'any_password',
-  //       passwordConfirmation: 'any_password'
-  //     }
-  //   }
-  //   const httpReponse = sut.handle(httpRequest)
-  //   expect(httpReponse.statusCode).toBe(400)
-  //   expect(httpReponse.body).toEqual(new MissingParamError('email'))
-  // })
+  test('Should return 400 if no password confirmation is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        email: 'any_email@mail.com'
+      }
+    }
+    const httpReponse = sut.handle(httpRequest)
+    expect(httpReponse.statusCode).toBe(400)
+    expect(httpReponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
 })
